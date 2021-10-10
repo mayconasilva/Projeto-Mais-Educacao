064746bbba8f20fd3c376af105112f67a901b600
mayconasilva <mayconsilva0381@gmail.com>
2021-10-10 14:14:51 -0300

    First Commit

---
+++ assets/scripts/datas.js
@@ -0,0 +1,14 @@
+function achardata(){
+//Pegando informações sobre o dia
+let Data = new Date()
+let dia = Data.getDate()
+let mes = Data.getMonth()
+let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+let days = Data.getDay()
+let dias = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarte-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
+let ano = Data.getFullYear()
+
+//Inserindo na página da web
+let info = document.getElementById("data")
+info.innerHTML= `Hoje são ${dia} de ${meses[mes]} de ${ano} | ${dias[days]}`
+}
